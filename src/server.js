f9b5a555590cc90eccbb7349d10699d0d7808548
afonsoDomingos <karinganastudio23@gmail.com>
2025-04-29 19:23:34 +0200

    RPA API V03 29ABRIL2025: Atualizando configuração de CORS

--- src/server.js
+++ src/server.js
@@ -10,7 +10,26 @@ const port = process.env.PORT || 5000;
 
 // Configurar o CORS para permitir requisições da origem http://localhost:3000 
 //app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(cors({ origin: 'https://apirpa.onrender.com' }));
+
+const cors = require('cors');
+
+// Defina o CORS para permitir requisições de ambas as origens
+const allowedOrigins = [
+  'https://apirpa.onrender.com',  // Backend (Render)
+  'https://recuperaaqui.vercel.app',  // Frontend (Vercel)
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // Se a origem estiver na lista de permitidos, permita
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
+//app.use(cors({ origin: 'https://apirpa.onrender.com' }));
 
 // Middleware para lidar com dados JSON
 app.use(express.json());
