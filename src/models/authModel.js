e7c7788487e499c88bbb9db0f128de1a107fc313
Vibe <52966432+afonsoDomingos@users.noreply.github.com>
2025-06-24 21:32:19 -0700

    RPA 23 - 25/06/2025: Update authModel.js

--- src/models/authModel.js
+++ src/models/authModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+//const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 const authSchema = new mongoose.Schema({
   nome: {
