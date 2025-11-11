c3f6962837e209209ee4d42bee81ad20cdfca1d2
afonsoDomingos <karinganastudio23@gmail.com>
2025-11-11 15:06:17 +0200

    RPAAPI  V40 - 11/11/2025:fix: adiciona rota PUT /api/anuncios/:id para atualização de anúncios

--- src/routes/anuncios.js
+++ src/routes/anuncios.js
@@ -4,6 +4,7 @@ const {
   criarAnuncio,
   meusAnuncios,
   anunciosAtivos,
+  atualizarAnuncio,
   removerAnuncio,
 } = require('../controllers/anuncioController');
 const verificarToken = require('../middleware/authMiddleware');
@@ -13,6 +14,7 @@ console.log('Rotas de anúncios carregadas');
 router.post('/', verificarToken, criarAnuncio);
 router.get('/meus', verificarToken, meusAnuncios);
 router.get('/ativos', anunciosAtivos);
+router.put('/:id', verificarToken, atualizarAnuncio); // <-- Adiciona esta linha
 router.delete('/:id', verificarToken, removerAnuncio);
 
 module.exports = router;
